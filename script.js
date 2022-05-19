1a24842d07c748f7f0cd94b0e8c157e02ba5aa64
DrMESAZIM <47475734+DrMESAZIM@users.noreply.github.com>
2022-05-19 04:24:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+document.querySelector('.btn').onclick=()=>{
+    document.querySelector('.cookies-box').classList.add('active')
+}
+
+
+document.querySelector('.clos').onclick=()=>{
+    document.querySelector('.cookies-box').classList.remove('active')
+}
